e3c09f6ca8d9369c1bf4c069a56d26b9bb2368c9
Hjalte <hjaltepind@gmail.com>
2022-04-26 12:23:12 +0200

    Added entails

--- Belief_base.py
+++ Belief_base.py
@@ -11,16 +11,21 @@ class belief_base:
         sentence = sentence.replace(' ', '')
         sentence = simplify(sentence)
         CNF = to_CNF(sentence)
-        if DPLL_satisfiable([x[0] for x in self.CNF], CNF):
+        if DPLL_satisfiable([x[i] for x in self.CNF for i in range(len(x))], CNF):
             self.base.append([sentence, score])
-            for element in CNF:
-                if not is_tautology(element):
-                    self.CNF.append([remove_duplicates(element), score])
+            for i in range(len(CNF)):
+                if is_tautology(CNF[i]):
+                    CNF.remove(CNF[i])
+                CNF[i] = remove_duplicates(CNF[i])
+            self.CNF.append(CNF)
         else:
             print('Sentence {} is incompatible with what is in the belief base'.format(sentence))
 
     def satisfiable(self, sentence):
-        return DPLL_satisfiable([x[0] for x in self.CNF], to_CNF(sentence))
+        return DPLL_satisfiable([x[i] for x in self.CNF for i in range(len(x))], to_CNF(sentence))
+
+    def entails(self, sentence):
+        return not self.satisfiable('-'+sentence)
 
     def print(self):
         if len(self.base) == 0:
@@ -31,7 +36,11 @@ class belief_base:
             for element in self.base:
                 print('{: <{padding}} | {}'.format(element[0], element[1], padding=padding))
             print('\n')
-            padding = max(len(max([x[0] for x in self.CNF], key=len)), len('Belief in CNF '))
+            padding = max(len(max([x[i] for x in self.CNF for i in range(len(x))], key=len)), len('Belief in CNF '))
             print('{:<{padding}} | Certainty:'.format('Belief in CNF ', padding = padding))
-            for element in self.CNF:
-                print('{:<{padding}} | {}'.format(element[0], element[1], padding=padding))
+            for i in range(len(self.CNF)):
+                belief = self.CNF[i]
+                for element in belief:
+                    print('{:<{padding}} | {}'.format(element, self.base[i][1], padding=padding))
+
+
--- tests/test_cases.py
+++ tests/test_cases.py
@@ -1,4 +1,5 @@
 from Belief_base import belief_base
+from utils import divide_sentence
 
 # Test cases found by inserting logical formulas in WolframAlpha using BooleanConvert[..., 'CNF']
 test1 = ['B<->(P_1|Q)', '-B|P_1|Q', 'B|-P_1', '-Q|B']
@@ -10,6 +11,7 @@ test6 = ['a->(s->(d<->m))', '-a|-s|-d|m', '-a|-s|-m|d']
 test7 = ['(a->(s|(t->h)))&((s->w)|q)', '-a|s|-t|h', '-s|w|q']
 test8 = ['(a->((s|(t->h_1))&(s->w)))|q', '-a|s|-t|h_1|q', '-a|-s|w|q']
 test9 = ['(a<->s_1)->(t_1->(a<->(s_1<->m)))', 'a|m|s_1|-t_1', 'm|-a|-s_1|-t_1']
+test10 = ['(P|R)->S']
 
 test = [test1, test2, test3, test4, test5, test6, test7, test8, test9]
 
@@ -17,13 +19,13 @@ x = belief_base()
 
 for i in range(len(test)):
     x.add(test[i][0], i+1)
-    """for j in range(len(test[i])-1):
+    for j in range(len(test[i])-1):
         try:
-            assert set([part for part in divide_sentence(test[i][j+1]) if part != '|']) in [set(n for n in divide_sentence(m[0]) if n != '|') for m in x.CNF], f'Failed on test {i+1}. Expected CNF form not found in belief base. Added belief: {test[i][0]}. Expected CNF part: {test[i][j+1]}'
+            assert set([part for part in divide_sentence(test[i][j+1]) if part != '|']) in [set(n for n in divide_sentence(m[i]) if n != '|') for m in x.CNF for i in range(len(m))], f'Failed on test {i+1}. Expected CNF form not found in belief base. Added belief: {test[i][0]}. Expected CNF part: {test[i][j+1]}'
         except AssertionError:
             x.print()
-            assert set([part for part in divide_sentence(test[i][j+1]) if part != '|']) in [set(n for n in divide_sentence(m[0]) if n != '|') for m in x.CNF], f'Failed on test {i+1}. Expected CNF form not found in belief base. Added belief: {test[i][0]}. Expected CNF part: {test[i][j+1]}'
-"""
+            assert set([part for part in divide_sentence(test[i][j+1]) if part != '|']) in [set(n for n in divide_sentence(m[i]) if n != '|') for m in x.CNF for i in range(len(m))], f'Failed on test {i+1}. Expected CNF form not found in belief base. Added belief: {test[i][0]}. Expected CNF part: {test[i][j+1]}'
+
 print('All test passed. Resulting belief base:\n')
 x.print()
 x.add('-S&-M',100)
