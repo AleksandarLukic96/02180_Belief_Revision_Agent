e1f4512f46cee158437e04917b7abae3520aa7d0
JHellum <LeoTheo1234>
2022-04-27 08:33:20 +0200

    More robustness in main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from Belief_base import *
 from utils import *
 
+def error(user_input):
+    print(f"No match for: \"{user_input}\"")
+
 if __name__ == "__main__":
 
     print("Program start")
@@ -26,57 +29,75 @@ if __name__ == "__main__":
 
         input_words = user_input.split()
         words = len(input_words)
-
-        if len(input_words) == 0:
-            print(input_words)
-
-        elif user_input.lower() == "exit":
+        if user_input.lower() == "exit":
             print("Thanks for playing")
             break
 
         elif input_words[0].upper() == "CREATE" and words == 2:
-            globals()[user_input.split()[1]] = belief_base()
+            if not input_words[1].isdigit():
+                globals()[input_words[1]] = belief_base()
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "EXPAND" and words == 4:
             name = input_words[1]
             sentence = input_words[2]
             priority_weight = input_words[3]
-            globals()[name].add(sentence, priority_weight)
+            if (globals().__contains__(name) and priority_weight.isdigit()):
+                globals()[name].add(sentence, priority_weight)
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "CONTRACT" and words == 3:
             name = input_words[1]
             sentence = input_words[2]
-            globals()[name].contract(sentence)
+            if (globals().__contains__(name)):
+                globals()[name].contract(sentence)
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "REVISION" and words == 4:
             name = input_words[1]
             sentence = input_words[2]
             priority_weight = input_words[3]
-            globals()[name].revision(sentence, priority_weight)
+            if (globals().__contains__(name) and priority_weight.isdigit()):
+                globals()[name].revision(sentence, priority_weight)
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "ENTAILS" and words == 3:
             name = input_words[1]
             sentence = input_words[2]
-            not_helper = "DOES NOT entail"
-            if globals()[name].entails(sentence):
-                not_helper = "entails"
-            print(f"The belief base: {name} {not_helper} the sentence: {sentence}")
+            if (globals().__contains__(name)):
+                not_helper = "DOES NOT entail"
+                if globals()[name].entails(sentence):
+                    not_helper = "entails"
+                print(f"The belief base: {name} {not_helper} the sentence: {sentence}")
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "SATISFIES" and words == 3:
             name = input_words[1]
             sentence = input_words[2]
-            not_helper = "DOES NOT satisfy"
-            if globals()[name].satisfiable(sentence):
-                not_helper = "satisfy"
-            print(f"The belief base: {name} {not_helper} the sentence: {sentence}")
+            if (globals().__contains__(name)):
+                not_helper = "DOES NOT satisfy"
+                if globals()[name].satisfiable(sentence):
+                    not_helper = "satisfy"
+                print(f"The belief base: {name} {not_helper} the sentence: {sentence}")
+            else:
+                error(user_input)
 
         elif input_words[0].upper() == "PRINT" and words == 2:
             name = input_words[1]
-            globals()[name].print()
+            if (globals().__contains__(name)):
+                globals()[name].print()
+            else:
+                error(user_input)
 
         elif user_input.upper() == "MASTER MIND":
             print("Not yet implemented")
             # TODO Implement
 
         else:
-            print(f"No match for: \"{user_input}\"")
+            error(user_input)
+
